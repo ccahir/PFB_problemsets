5266986ea0213fb2097723b356775c41669b0687
pfb19 <pfb19@info19.cshl.edu>
2023-10-17 11:25:01 -0400

    adding problem set python 01

---
+++ problem_set_python_01/my_favorites.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+
+#variables
+my_name = "Clare"
+my_fav_activity = "Running"
+
+#prints my name, favorite, color, favorite activity, and favorite animal
+print(f"My name: {my_name}")  #trying out the f string format
+print("My favorite color: Green")
+print("My favorite activity:", my_fav_activity)
+print("My favorite animal: Frog")
---
+++ problem_set_python_01/sys_my_favorites.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+import sys
+
+name = sys.argv[1] #get first command line parameter
+color = sys.argv[2] #get second command line parameter
+activity = sys.argv[3] #get third command line parameter
+animal = sys.argv[4] #get fourth command line parameter
+
+#print message to screen
+print(name,"+",color,"+",activity,"+",animal)
