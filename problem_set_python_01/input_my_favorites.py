02412df229f50424b9d525ef06b0dc506b5e65fe
pfb19 <pfb19@info19.cshl.edu>
2023-10-17 11:42:53 -0400

    updating problem set python 01

---
+++ problem_set_python_01/input_my_favorites.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3 
+
+#get user input for variables name, color, activity, animal
+name = input("Input your name:")
+color = input("Input your favorite color:")
+activity = input("Input your favorite activity:")
+animal = input("Input your favorite animal:")
+
+#print message to screen
+print("Your name is:", name)
+print("Your favorite color is:", color)
+print("Your favorite activity is:", activity)
+print("Your favorite animal is:", animal)
